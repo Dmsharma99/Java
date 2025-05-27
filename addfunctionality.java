e0d648fedd95574eec177b826f04a03289704a39
Durgesh M Sharma <149234216+Dmsharma99@users.noreply.github.com>
2025-05-27 17:13:39 +0530

    Update and rename instanceVariable1.java to addfunctionality.java

---
+++ addfunctionality.java
@@ -0,0 +1,13 @@
+// Adding Functionality in Java Classes
+
+// Why we use Obejct Oriented Programming?
+// Object-oriented programming allows developers to model real-life scenarios.
+// In our example, we use a car model with properties such as doors, engine, driver, and speed. 
+// Each property is encapsulated within the class,
+// and methods (getters and setters) control access.
+
+
+/*
+Car.java
+*/
+

0b2083be2388991ef5f95c33f839a38ed40304ea
Durgesh M Sharma <149234216+Dmsharma99@users.noreply.github.com>
2025-05-27 12:56:12 +0530

    Update instanceVariable.java

--- instanceVariable.java
+++ instanceVariable.java
@@ -1,1 +1,34 @@
+/* Instance Variable default values */
+
+/* new java mocule with the name of Car.java */
+
+public class Car {
+
+public String doors;
+public String engine;
+public String driver;
+public int speed;
+
+public float test;
+public boolean test2;
+
+}
+
+/* Insdie the main.java */
+
+public class Main {
+public static void main(String[] args){
+
+  Car car = new Car();
+  System.out.println(car.test);
+  System.out.println(car.test2);
+}
+}
+
+/*
+O/p  :    0.0
+          false
+*/
+
+
 
